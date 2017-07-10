3e8cfa68f86057254ed2dc94f993367249ab604e
sxy <songxiaoyuan>
2017-07-10 15:38:36 +0800

    add the data

--- HelloWorld/HelloWorld/getmddata.py
+++ HelloWorld/HelloWorld/getmddata.py
@@ -1,16 +1,8 @@
 import os
+import threading
+from time import ctime,sleep
 from subprocess import Popen,PIPE
-# md = ":\\md\\mdBasic.exe"
-# # p = Popen(md,stdout = PIPE,bufsize =10000)
-# # for line in iter(p.stdout.readline,''):
-# # 	line = line.split(",")
-# # 	print type(line)
-# # 	print line
-# # os.system(md)
-# path =  os.getcwd()
-# path = path + "\md\mdBasic.exe"
-# # print path
-# os.system(path)
+
 def GetMDData(dic):
 	# print "start to get the md data and insert to the dic"
 	path = os.getcwd()+ "\md\mdBasic.exe"
@@ -19,13 +11,33 @@ def GetMDData(dic):
 		line = line.split(",")
 		if len(line) ==9:
 			instrumentId = line[1].strip()
+			# print instrumentId
 			if instrumentId in dic:
 				dic[instrumentId].append(line)
 			else:
 				dic[instrumentId] = [line]
+	p.stdout.close()
+	p.terminate()
 
+def printDic(dic):
+	while True:
+		sleep(5)
+		print dic
+
+def main():
+	dic = dict()
+	threads = []
+	t1 = threading.Thread(target=GetMDData,args=(dic,))
+	threads.append(t1)
+	t2 = threading.Thread(target=printDic,args=(dic,))
+	threads.append(t2)
+	print dic
+	for t in threads:
+	    t.setDaemon(True)
+	    t.start()
+	print "out"
+	t1.join()
+	t2.join()
 
 if __name__ == '__main__':
-	dic = dict()
-	GetMDData(dic)
-	print dic
+	main()
